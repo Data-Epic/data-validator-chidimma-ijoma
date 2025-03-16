f55f302f82e995ca42c36b4f71550ef578c98bc3
Chidimma Ijoma <nevusijoma@gmail.com>
2025-03-16 19:36:22 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+
 setup(
     name='datavalidator',
     version='0.1',
@@ -6,16 +7,15 @@ setup(
     install_requires=[],
     author='Chidimma Ijoma',
     author_email='nevusijoma@gmail.com',
-    description='A simple data validation package'
+    description='A simple data validation package',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Data-Epic/data-validator-chidimma-ijoma",
-    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # Use the correct classifier
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    license="MIT",  # Specify the license here
+    license="MIT",
 )
